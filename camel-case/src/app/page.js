8d50171802694b70810f697d50facacf7cf144f5
evanjschmitt <evanjschmitt@gmail.com>
2024-10-17 21:58:44 -0400

    A little Camel Styling

--- camel-case/src/app/page.js
+++ camel-case/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Camel from "@/components/Camel";
+import Styles from "@/app/page.module.css"
 
 export default function Home() {
   return (
@@ -8,7 +9,35 @@ export default function Home() {
       <h1>Camels in Costumes</h1>
       <p>Look at these Cute Camels!</p>
       <h3>Camel Showcase</h3>
+      <section className={Styles.camelShowcase}>
+
+      </section>
       <Camel 
+      title='Camel 01'
+      desc= 'Camel With Cowboy Hat'
+      img='camel01'
+      />
+      <Camel 
+      title='Camel 01'
+      desc= 'Camel With Cowboy Hat'
+      img='camel01'
+      /><Camel 
+      title='Camel 01'
+      desc= 'Camel With Cowboy Hat'
+      img='camel01'
+      /><Camel 
+      title='Camel 01'
+      desc= 'Camel With Cowboy Hat'
+      img='camel01'
+      /><Camel 
+      title='Camel 01'
+      desc= 'Camel With Cowboy Hat'
+      img='camel01'
+      /><Camel 
+      title='Camel 01'
+      desc= 'Camel With Cowboy Hat'
+      img='camel01'
+      /><Camel 
       title='Camel 01'
       desc= 'Camel With Cowboy Hat'
       img='camel01'
